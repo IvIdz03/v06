37a1edf130adcab8c231494468b937936be99b14
IvIdz03 <ividzano@vsite.hr>
2025-05-20 19:51:54 +0200

    Rj vj 6

--- app/app.h
+++ app/app.h
@@ -1,6 +1,40 @@
 #pragma once
 
-namespace vsite::oop::v6 
+namespace vsite::oop::v6
 {
+	template <typename T>
 
+	T minimum(T a, T b)
+	{
+		if (a < b)
+			return a;
+		return b;
+	}
+
+	template <typename T, int X>
+
+	class fixed_array
+	{
+	private:
+		T array[X];
+	public:
+		unsigned long long size()
+		{
+			return X;
+		}
+
+		T operator[](int a)
+		{
+			return array[a];
+		}
+	};
+
+	template <typename T>
+
+	class point
+	{
+	public:
+		T a;
+		T b;
+	};
 }
